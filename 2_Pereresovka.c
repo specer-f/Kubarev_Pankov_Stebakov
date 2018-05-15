094363b5223d05110b8bf8369ad19ce422313842
Alex-Green <bolt.sashka2015@yandex.ru>
2018-05-15 14:12:20 +0300

    2_Pereresovka.c

---
+++ 2_Pereresovka.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ncurses.h>
+
+struct Snake
+{
+	int x[100];
+	int y[100];
+};
+
+int main()
+{
+	struct Snake snake;
+	int dop_x. dop_y;
+	for (int i = 0; i < 100; i++) {
+		dop_x = snake.x[i];
+		dop_y = snake.y[i];
+		if (i == 0) { 
+			snake.x[i]++;
+			snake.y[i]++;
+		}
+		else {
+			
+	}
+
+
+	return 0;
+}
